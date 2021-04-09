1a9025a38a4515deb2fbeb29f06eabc61a21b206
EL001 <59197738+EL001@users.noreply.github.com>
2021-04-09 09:46:35 +0100

    Add files via upload

--- person.py
+++ person.py
@@ -1,17 +1,17 @@
 class Person:
 
-    def __init__(self, age, weight, height, gender):        
+    def __init__(self, age: int, weight: float, height: float, gender: type):        
         self.age = age
         self.weight = weight
         self.height = height
         self.gender = gender
         
 
-    def bmi(self):
+    def bmi(self) -> float:
         return round(self.weight / (self.height ** 2), 2)
         
 
-    def calories(self):
+    def calories(self) -> float:
         if self.gender == 'male':
             return round((10 * self.weight) + (625 * self.height) - (5 * self.age) + 5, 2)
         else:
@@ -19,11 +19,11 @@ class Person:
                 return round((10 * self.weight) + (625 * self.height) - (5 * self.age) - 161, 2)
 
 
-    def healthy_weight(self):
+    def healthy_weight(self) -> float:
         return round(18.5 * (self.height ** 2), 2), round(24.9 * (self.height ** 2), 2)
 
     
-    def ideal_weight(self):
+    def ideal_weight(self) -> float:
         if self.gender == 'male':
             if self.height <= 1.524:
                 return 50
@@ -36,9 +36,9 @@ class Person:
                 else:
                     return round(45.5 + (2.3 * ((self.height - 1.524) / 0.0254)), 2)
     
-
     
 enoch = Person(26, 65, 1.84, 'male')
+
 
 person = 'Enoch'
 cal = (enoch.calories())
@@ -47,13 +47,7 @@ ideal_w = (enoch.ideal_weight())
 healthy_w = (enoch.healthy_weight())
 
 
-
-print ('For a person named {}, body mass index is {}, which indicates an ideal weight range of {} and a healthy weight of {}.'
-        'The minimum amount of the calories needed each when the body is at rest is {}'
-                .format(person, bmi, healthy_w, ideal_w, cal))
-
-
-
-
-'''print ('For {}, with parameters age {}, weight {}kg, height {}m and gender {} '
-      .format(person, self.age, self.weight, self.height, self.gender ))'''
+print ( 'For a person named {}, the body mass index is {},'
+         '\n''this indicates an ideal weight range of {}kg and a healthy weight of {}kg.'
+           '\n''The minimum amount of the calories needed each when the body is at rest is {}kcal/day'
+               .format(person, bmi, healthy_w, ideal_w, cal))
